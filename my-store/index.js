37b2d06336235a6ff3b312749c27c215b6b7c953
Jimmy Joel P. Malqui <jpmjim@gmail.com>
2021-10-13 16:23:00 -0500

    deploy con heroku

--- my-store/index.js
+++ my-store/index.js
@@ -5,7 +5,7 @@ const routerApi = require('./routes');
 const { logErros, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //captura de la informacion POST "el middleware "
 app.use(express.json());
@@ -13,7 +13,7 @@ app.use(express.json());
 const whitelist = ['http://localhost:5500', 'https://myapp.co'];
 const options = {
   origin: (origin, callback) => {
-    if (whitelist.includes(origin)) {
+    if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('no permitido'));
